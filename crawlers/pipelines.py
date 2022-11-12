9e590dbe9a35b080bfb9b8459cf5194ef7cba0e8
jeron7 <jeronimo.araujo@ccc.ufcg.edu.br>
2022-11-12 13:48:12 -0300

    Fix pipeline that was writting csv header each time pipelines runs

--- crawlers/pipelines.py
+++ crawlers/pipelines.py
@@ -1,12 +1,13 @@
 from itemadapter import ItemAdapter
 import csv
+import os
 
 class CsvWriterPipeline:
 	def open_spider(self, spider):
 		self.file_name = 'data/deputies.csv';
 		self.file = open(self.file_name, 'a+')
 		self.csvwriter = csv.writer(self.file)
-		self.has_header = False
+		self.has_header = os.stat(self.file_name).st_size != 0
 
 	def close_spider(self, spider):
 		self.file.close()
